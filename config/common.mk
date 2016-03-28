BlissRoms/platform_vendor_bliss

vendor/aosp: Add LatinIME dictionaries support
Thanks to CyanogenMod

Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>

common.mk
@@ -20,6 +20,9 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
 
+# Include LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/dictionaries
+
 # init.d support
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/bin/sysinit:system/bin/sysinit \

TeamFlex/vendor_flex

Change-Id: Id68d6005fd7b43b0d98e7988dd4db9eaf2d59006
Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
Conflicts:
	config/common.mk
	overlay/dictionaries/packages/inputmethods/LatinIME/java/res/raw/main_cs.dict
	overlay/dictionaries/packages/inputmethods/LatinIME/java/res/raw/main_da.dict
	overlay/dictionaries/packages/inputmethods/LatinIME/java/res/raw/main_el.dict
	overlay/dictionaries/packages/inputmethods/LatinIME/java/res/raw/main_fi.dict
	overlay/dictionaries/packages/inputmethods/LatinIME/java/res/raw/main_hr.dict
	overlay/dictionaries/packages/inputmethods/LatinIME/java/res/raw/main_iw.dict
	overlay/dictionaries/packages/inputmethods/LatinIME/java/res/raw/main_nb.dict
	overlay/dictionaries/packages/inputmethods/LatinIME/java/res/raw/main_nl.dict
	overlay/dictionaries/packages/inputmethods/LatinIME/java/res/raw/main_pt_pt.dict
	overlay/dictionaries/packages/inputmethods/LatinIME/java/res/raw/main_sv.dict

config/common.mk
@@ -118,6 +118,9 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/flex/overlay/common
 
+# Include LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/flex/overlay/dictionaries
+
 #Bootanimation
  $(LOCAL_PATH)/media/bootanimation.zip:system/media/bootanimation.zip
 