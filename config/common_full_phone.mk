ab57d83396dfcb99de45a8cfc10df4651ce64ff3
Akhil Narang <akhilnarang.1999@gmail.com>
2016-05-27 00:43:44 -0400

    vendor: this is FlayrOS!
      * Don't mess with teh DPI post build
    
    Change-Id: Ib18b4927abe17eff49b04aae1719bfa0576de86b
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -3,11 +3,11 @@ $(call inherit-product, vendor/flex/config/common.mk)
 
 # World APN list
 PRODUCT_COPY_FILES += \
-    vendor/slim/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/flex/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # Selective SPN list for operator number who has the problem.
 PRODUCT_COPY_FILES += \
-    vendor/slim/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+    vendor/flex/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
 
 # Telephony packages
 PRODUCT_PACKAGES += \
